a4feaa19459237a8ed3f6abfc3fa392ca0311aa2
SnakCat <snakkdss@gmail.com>
2025-03-03 22:35:54 +0300

    initial commit

---
+++ iOSTZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTZ
+//
+//  Created by Дмитрий Трушин on 03/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
